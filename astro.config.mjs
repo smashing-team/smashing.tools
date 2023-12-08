68b8432560e201f4d031f4a63e8bca0381226b1f
Evren <evren@192.168.1.107>
2023-12-08 15:37:44 +0300

    react was added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +15,7 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    react()
   ],
   output: "hybrid"
 });
