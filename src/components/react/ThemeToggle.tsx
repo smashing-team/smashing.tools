e9c517458877660d8b5ffe82a8d737bcd10e4178
Nedim Arabacı <nedimarabaci@gmail.com>
2023-12-18 19:49:32 +0300

    mobile menu

--- src/components/react/ThemeToggle.tsx
+++ src/components/react/ThemeToggle.tsx
@@ -25,77 +25,41 @@ export function ThemeToggle() {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
+          width="24"
+          height="24"
           viewBox="0 0 24 24"
-          strokeWidth="1.5"
+          strokeWidth="2"
           stroke="currentColor"
           fill="none"
           strokeLinecap="round"
           strokeLinejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path
-            d="M12 1.992a10 10 0 1 0 9.236 13.838c.341 -.82 -.476 -1.644 -1.298 -1.31a6.5 6.5 0 0 1 -6.864 -10.787l.077 -.08c.551 -.63 .113 -1.653 -.758 -1.653h-.266l-.068 -.006l-.06 -.002z"
-            strokeWidth="0"
-            fill="currentColor"
-          />
+          <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+          <path d="M12 5l0 .01" />
+          <path d="M17 7l0 .01" />
+          <path d="M19 12l0 .01" />
+          <path d="M17 17l0 .01" />
+          <path d="M12 19l0 .01" />
+          <path d="M7 17l0 .01" />
+          <path d="M5 12l0 .01" />
+          <path d="M7 7l0 .01" />
         </svg>
       ) : (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
+          className="h-5 w-5"
+          width="24"
+          height="24"
           viewBox="0 0 24 24"
-          strokeWidth="1.5"
+          strokeWidth="2"
           stroke="currentColor"
           fill="none"
           strokeLinecap="round"
           strokeLinejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path
-            d="M12 19a1 1 0 0 1 .993 .883l.007 .117v1a1 1 0 0 1 -1.993 .117l-.007 -.117v-1a1 1 0 0 1 1 -1z"
-            strokeWidth="0"
-            fill="currentColor"
-          />
-          <path
-            d="M18.313 16.91l.094 .083l.7 .7a1 1 0 0 1 -1.32 1.497l-.094 -.083l-.7 -.7a1 1 0 0 1 1.218 -1.567l.102 .07z"
-            strokeWidth="0"
-            fill="currentColor"
-          />
-          <path
-            d="M7.007 16.993a1 1 0 0 1 .083 1.32l-.083 .094l-.7 .7a1 1 0 0 1 -1.497 -1.32l.083 -.094l.7 -.7a1 1 0 0 1 1.414 0z"
-            strokeWidth="0"
-            fill="currentColor"
-          />
-          <path
-            d="M4 11a1 1 0 0 1 .117 1.993l-.117 .007h-1a1 1 0 0 1 -.117 -1.993l.117 -.007h1z"
-            strokeWidth="0"
-            fill="currentColor"
-          />
-          <path
-            d="M21 11a1 1 0 0 1 .117 1.993l-.117 .007h-1a1 1 0 0 1 -.117 -1.993l.117 -.007h1z"
-            strokeWidth="0"
-            fill="currentColor"
-          />
-          <path
-            d="M6.213 4.81l.094 .083l.7 .7a1 1 0 0 1 -1.32 1.497l-.094 -.083l-.7 -.7a1 1 0 0 1 1.217 -1.567l.102 .07z"
-            strokeWidth="0"
-            fill="currentColor"
-          />
-          <path
-            d="M19.107 4.893a1 1 0 0 1 .083 1.32l-.083 .094l-.7 .7a1 1 0 0 1 -1.497 -1.32l.083 -.094l.7 -.7a1 1 0 0 1 1.414 0z"
-            strokeWidth="0"
-            fill="currentColor"
-          />
-          <path
-            d="M12 2a1 1 0 0 1 .993 .883l.007 .117v1a1 1 0 0 1 -1.993 .117l-.007 -.117v-1a1 1 0 0 1 1 -1z"
-            strokeWidth="0"
-            fill="currentColor"
-          />
-          <path
-            d="M12 7a5 5 0 1 1 -4.995 5.217l-.005 -.217l.005 -.217a5 5 0 0 1 4.995 -4.783z"
-            strokeWidth="0"
-            fill="currentColor"
-          />
+          <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
         </svg>
       )}
     </button>
