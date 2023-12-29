ea6719113f34a8e857a4e3554463cacb02e5d1a9
Nedim Arabacı <nedimarabaci@gmail.com>
2023-12-29 04:11:07 +0300

    Add sitemap configuration and link to BaseHead component

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,11 @@ export default defineConfig({
   },
   integrations: [
     mdx(),
-    sitemap(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
     tailwind({
       applyBaseStyles: false,
     }),
