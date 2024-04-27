515f8e4eb830b72d6cb5afaf60640ab24a8e41f5
Nedim Arabacı <nedimarabaci@gmail.com>
2024-04-27 14:37:01 +0300

    cleanup

--- app/(app)/[category]/[slug]/page.tsx
+++ app/(app)/[category]/[slug]/page.tsx
@@ -198,12 +198,29 @@ export default async function ToolDetail({
                   <Link
                     target="_blank"
                     href={websiteUrl.toString()}
-                    className="flex items-center rounded-3xl bg-zinc-50 px-5 h-12 text-base font-medium text-zinc-900 ring-inset border border-zinc-300 hover:bg-zinc-100/75 dark:border-zinc-100 dark:hover:bg-white/95 shadow-xl"
+                    className="flex items-center rounded-3xl bg-zinc-50 dark:bg-white px-5 h-12 text-base font-medium text-zinc-900 ring-inset border border-zinc-300 hover:bg-zinc-100/75 dark:border-zinc-100 dark:hover:bg-white/90 shadow-xl"
                   >
                     <IconExternalLink className="relative -left-0.5 mr-1.5 size-5 text-zinc-400 dark:text-zinc-800" />
                     Website
                   </Link>
                 </li>
+                {item.buyLink && (
+                  <li>
+                    <Link
+                      target="_blank"
+                      href={item.buyLink.toString()}
+                      className="relative inline-flex items-center justify-center px-5 h-12 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group"
+                    >
+                      <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700 pointer-events-none"></span>
+                      <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+                      <span className="relative text-white flex items-center">
+                        <IconShoppingCart className="relative -left-0.5 mr-1.5 size-5" />{" "}
+                        Buy now
+                        <IconArrowRight className="ml-1.5 size-5" />
+                      </span>
+                    </Link>
+                  </li>
+                )}
                 {repoUrl && (
                   <li>
                     <Link
@@ -213,23 +230,6 @@ export default async function ToolDetail({
                     >
                       <IconBrandGithub className="relative -left-0.5 mr-1.5 size-5" />
                       Repository
-                    </Link>
-                  </li>
-                )}
-                {item.buyLink && (
-                  <li>
-                    <Link
-                      target="_blank"
-                      href={item.buyLink.toString()}
-                      className="relative inline-flex items-center justify-center px-5 h-12 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500"
-                    >
-                      <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700 pointer-events-none"></span>
-                      <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
-                      <span className="relative text-white flex items-center">
-                        <IconShoppingCart className="relative -left-0.5 mr-1.5 size-5" />{" "}
-                        Buy now
-                        <IconArrowRight className="ml-1.5 size-5" />
-                      </span>
                     </Link>
                   </li>
                 )}
@@ -270,7 +270,7 @@ export default async function ToolDetail({
                 <Link
                   target="_blank"
                   href={item.buyLink.toString()}
-                  className="relative flex mb-4 no-underline items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500"
+                  className="relative flex mb-4 no-underline items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group"
                 >
                   <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
                   <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
--- components/button.tsx
+++ components/button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-8 rounded-lg px-3 text-xs",
-        lg: "h-11 rounded-lg px-8",
+        lg: "h-11 rounded-full px-8",
         icon: "h-10 w-10",
       },
     },
