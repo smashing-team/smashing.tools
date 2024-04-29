bc77d3a775af9c422a870cb9cb75b74433f2d390
Nedim Arabacı <nedimarabaci@gmail.com>
2024-04-29 11:29:49 +0300

    auth redirect

--- app/(app)/api/auth/signin/github/route.ts
+++ app/(app)/api/auth/signin/github/route.ts
@@ -1,19 +1,15 @@
 import { createClient } from "@/utils/supabase/server";
+import { getBaseUrl } from "@/utils/url";
 import { redirect } from "next/navigation";
 import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const redirectTo =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/api/auth/callback"
-      : "https://smashing.tools/api/auth/callback";
-
   const supabase = createClient();
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
-      redirectTo,
+      redirectTo: `${getBaseUrl()}/api/auth/callback`,
     },
   });
 
