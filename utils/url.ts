56d9268547a272a2c29d5c4a21dee6c977705bb0
Nedim Arabacı <nedimarabaci@gmail.com>
2024-04-29 11:25:28 +0300

    baseUrl

--- utils/url.ts
+++ utils/url.ts
@@ -1,7 +1,9 @@
 export const getBaseUrl = (): string => {
-  if (process.env.NODE_ENV === "development") {
-    return `http://localhost:${process.env.PORT ?? 3000}`;
+  if (process.env.URL) {
+    return process.env.URL;
   }
-
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:3000";
+  }
   return "https://smashing.tools";
 };
